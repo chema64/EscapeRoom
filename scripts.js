38d4cbc1e277eb11c94c33b3c1eb1095a283ab70
chema64 <chema64@gmail.com>
2024-03-26 09:47:53 +1000

    Trying to implement Ruffle autoplay config

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@ window.RufflePlayer = window.RufflePlayer || {};
 window.addEventListener("load", (event) => {
     const ruffle = window.RufflePlayer.newest();
     const player = ruffle.createPlayer();
+    player.config = {
+        autoplay: "auto"
+    };
     const container = document.getElementById("container");
     container.appendChild(player);
     player.load("EscapeRoom11.swf");
