ab57ef65b8118ce599b70efb4cdca62d3d910ccb
chema64 <chema64@gmail.com>
2024-03-26 14:36:13 +1000

    Code cleaned up. Code refactored all unused code and commented out code removed.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
 window.RufflePlayer = window.RufflePlayer || {};
 window.addEventListener("load", (event) => {
-    // window.RufflePlayer.config = {
-    //     // Start playing the content automatically, without audio if the browser in use does not allow audio to autoplay
-    //    "autoplay": "on",
-    // }
+
     const ruffle = window.RufflePlayer.newest();
     
     const player = ruffle.createPlayer();
