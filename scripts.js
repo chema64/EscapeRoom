d9495504b3121b285f167013bf92fb995eb0dd48
chema64 <chema64@gmail.com>
2024-03-26 11:18:05 +1000

    heading removed from webpage. Trying again to use player.config with autoplay.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 window.RufflePlayer = window.RufflePlayer || {};
 window.addEventListener("load", (event) => {
-    window.RufflePlayer.config = {
-        // Start playing the content automatically, without audio if the browser in use does not allow audio to autoplay
-       "autoplay": "on",
-    }
+    // window.RufflePlayer.config = {
+    //     // Start playing the content automatically, without audio if the browser in use does not allow audio to autoplay
+    //    "autoplay": "on",
+    // }
     const ruffle = window.RufflePlayer.newest();
 
     const player = ruffle.createPlayer();
+    player.config ={
+        autoplay: "on",
+    }
     const container = document.getElementById("container");
     container.appendChild(player);
     player.load("EscapeRoom11.swf");
